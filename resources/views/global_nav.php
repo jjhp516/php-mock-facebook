8a018b9a42c98f59d32024d9c0c541ebda0abd1c
Jason Park <803456@ACDM.DS.SAIT.CA>
2018-11-06 12:35:53 -0700

    set up basic page layout and nav

---
+++ resources/views/global_nav.php
@@ -0,0 +1,36 @@
+<nav class="d-flex">
+    <a href="#" class="mr-2"><i class="fab fa-facebook"></i></a>
+
+
+    <div class="search-control">
+        <div class="search-bar d-flex">
+            <input type="search" id="site-search" name="search-bar"
+                    placeholder="Search"
+                    aria-label="Search through site content">
+
+            <button><i class="fas fa-search"></i></button>
+        </div>
+    </div>
+
+
+    <div class="d-flex">
+        <div class="nav-links">
+                <a class="user-link" href="#">
+                    
+                </a>
+                <a href="#">Home</a>
+                <a href="#">Find Friends</a>
+        </div>
+        
+        <div class="buttons btnSet1 mr-2">
+                <button><i class="fas fa-user-friends fa-lg"></i></button>
+                <button><i class="fab fa-facebook-messenger fa-lg"></i></button>
+                <button><i class="fas fa-bell fa-lg"></i></button>
+        </div>
+
+        <div class="buttons btnSet2">
+                <button><i class="fas fa-question-circle fa-lg"></i></i></button>
+                <button><i class="fas fa-caret-down"></i></button>
+        </div>
+    </div>                
+</nav>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fakebook</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="../../css/app.css">
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            @include('global_nav')
+        </header>
+
+        <div class="sidebar left-side">
+        </div>
+
+        <main>
+        </main>
+
+        <div class="sidebar right-side">
+        </div>
+
+        <footer>
+        </footer>
+    </div>
+</body>
+</html>
