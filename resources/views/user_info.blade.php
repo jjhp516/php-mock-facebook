4b5aa7a47c9773fda347e21be80921042578d036
jjhp516 <jjhp516@gmail.com>
2018-11-08 04:51:00 -0700

    i think im gonna throw up

---
+++ resources/views/user_info.blade.php
@@ -0,0 +1,15 @@
+<div class="user-info placard">
+    <div class="info-heading d-flex">
+        <div class="blue-circle d-flex mr-2">
+            <i class="fas fa-globe-americas fa-sm"></i>
+        </div>
+        <p>Intro</p> 
+    </div>
+
+    <ul class="intro">
+        <li><i class="fas fa-briefcase mr-2"></i><?php echo $user->profile->occupation ?></li>
+        <li><i class="fas fa-graduation-cap mr-2"></i><?php echo $user->profile->education ?></li>
+        <li><i class="fas fa-map-marker-alt mr-2"></i> Lives in <?php echo $user->profile->location ?></li>
+        <li><i class="fas fa-map-marker-alt mr-2"></i> From <?php echo $user->profile->hometown ?></li>
+    </ul>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,9 @@
 @extends('layout')
 
+@section('left-sidebar')
+    @include('user_info')
+@endsection
+
 @section('main-content')
     @include('timeline_posts')
 @endsection
